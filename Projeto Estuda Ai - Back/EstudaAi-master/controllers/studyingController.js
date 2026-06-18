1887c848c0426696902f52c7bf86f41942062670
Aluno IFPR <aluno@ifpr.edu.br>
2026-06-18 10:34:23 -0300

    Primeiro Commit

---
+++ Projeto Estuda Ai - Back/EstudaAi-master/controllers/studyingController.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+const Studying = require('../model/Studying');
+
+const createStudying = async (req,res) => {
+    try{
+        const {nome, concluida} = req.body;
+        const existing = await Studying.findOne({nome});
+        if(existing){
+            return res.status(400).json({message:'Não foi possível favoritar a matéria'})
+        }
+        const studying = await Studying.create({nome, concluida});
+        res.status(201).json(studying);
+    }catch(error){
+        res.status(500).json({message:'Falha ao criar favorito!', error: error.message});
+    }
+}
+
+const getStudying = async (req, res) => {
+    try{
+        const studying = await Studying.find();
+        if(studying.length == 0){
+           return res.status(404).json('Nenhum favorito foi adicionado ainda');
+        }
+        res.status(200).json(studying);
+    } catch(error){
+        res.status(500).json({message:'Erro ao pegar os favoritos!', error: error.message});
+    }
+}
+
+const updateStudyingById = async (req,res) => {
+        try{
+            const {id} = req.params;
+            const {concluida} = req.body;
+            if(!mongoose.Types.ObjectId.isValid(id)){
+                return res.status(400).json({message:'Id inválido'})
+            }
+            const studying = await Studying.findByIdAndUpdate(
+                id, {concluida} , {new: true}
+            );
+            if(!studying){
+                return res.status(404).json({message:'Matéria não foi encontrado!'})
+            }
+            res.status(200).json(studying);
+        }catch(error){
+            res.status(500).json({
+                message: 'Erro ao atualizar matéria',
+                error: error.message
+            });
+        }
+};
+
+
+const deleteStudyingById = async (req, res) => {
+        try{
+            const {id} =  req.params;
+            if(!mongoose.Types.ObjectId.isValid(id)){
+               return res.status(400).json({message:'ID inválido'});
+            }
+            const studying = await Studying.findByIdAndDelete(id);
+            if(!studying){
+               return res.status(404).json({message:'Nenhuma matéria para deletar foi encontrada!'})
+            }
+            res.status(200).json(studying)
+        }catch(error){
+            res.status(500).json({message:'Erro ao deletar matéria!', error: error.message})
+        }
+    }
+
+module.exports = {
+    createStudying,
+    updateStudyingById,
+    deleteStudyingById,
+    getStudying
+}    
