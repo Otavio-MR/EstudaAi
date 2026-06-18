444fc300561c395a51d36c937a0e221f895a639a
João Victor Souza Da Silva <joaovs6401@gmail.com>
2026-06-18 09:47:05 -0300

    Delete EstudaAi-master directory

--- EstudaAi-master/routes/studyingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const {createStudying, getStudying, updateStudyingById, deleteStudyingById} = require('../controllers/studyingController')
-
-router.post("/studying", createStudying);
-router.get('/studying', getStudying);
-router.patch('/studying/:id', updateStudyingById);
-router.delete('/studying/:id', deleteStudyingById);
